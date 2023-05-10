a491fc44418206ef407f9fbdf932574301755a96
ManuelAmaya <manuel.amaya.acosta@hotmail.com>
2023-05-10 12:44:36 -0600

    Se configura POJO de las clases Carrito y Detalle_Carrito

--- src/main/java/com/losdelfines/backend/models/Carrito.java
+++ src/main/java/com/losdelfines/backend/models/Carrito.java
@@ -1,5 +1,51 @@
 package com.losdelfines.backend.models;
 
+
 public class Carrito {
-    
-}
+
+	private long id;
+	private long usuario_id;
+	private static long total = 0;
+
+	public Carrito(long id, long usuario_id) {
+		this.id = id;
+		this.usuario_id = usuario_id;
+		total++;
+		id = Carrito.total;
+	}// constructor
+
+	public Carrito() {
+		total++;
+		id = Carrito.total;
+	}// constructor default
+
+	public long getId() {
+		return id;
+	}// getId
+
+	public void setId(long id) {
+		this.id = id;
+	}// setId
+
+	public long getUsuario_id() {
+		return usuario_id;
+	}// getUsuario_id
+
+	public void setUsuario_id(long usuario_id) {
+		this.usuario_id = usuario_id;
+	}// setUsuario_id
+
+	public static long getTotal() {
+		return total;
+	}// getTotal
+
+	public static void setTotal(long total) {
+		Carrito.total = total;
+	}// setTotal
+
+	@Override
+	public String toString() {
+		return "Carrito [id=" + id + ", usuario_id=" + usuario_id + "]";
+	}// toString
+
+}// Carrito
--- src/main/java/com/losdelfines/backend/models/Detalle_carrito.java
+++ src/main/java/com/losdelfines/backend/models/Detalle_carrito.java
@@ -2,4 +2,76 @@ package com.losdelfines.backend.models;
 
 public class Detalle_carrito {
     
+	private long id;
+	private long productos_id;
+	private long carrito_id;
+	private long cantidad;
+	private static long total = 0;
+	public Detalle_carrito(long id, long productos_id, long carrito_id, long cantidad) {
+		this.id = id;
+		this.productos_id = productos_id;
+		this.carrito_id = carrito_id;
+		this.cantidad = cantidad;
+		total++;
+		id = Detalle_carrito.total;
+	}// constructor 
+	
+	public Detalle_carrito() {
+		total++;
+		id = Detalle_carrito.total;
+	}// constructor default
+
+	public long getId() {
+		return id;
+	}//getId
+
+	public void setId(long id) {
+		this.id = id;
+	}//setId
+
+	public long getProductos_id() {
+		return productos_id;
+	}//getProductos_id
+
+	public void setProductos_id(long productos_id) {
+		this.productos_id = productos_id;
+	}//setProductos_id
+
+	public long getCarrito_id() {
+		return carrito_id;
+	}//getCarrito_id
+
+	public void setCarrito_id(long carrito_id) {
+		this.carrito_id = carrito_id;
+	}//setCarrito_id
+
+	public long getCantidad() {
+		return cantidad;
+	}//getCantidad
+
+	public void setCantidad(long cantidad) {
+		this.cantidad = cantidad;
+	}//setCantidad
+
+	public static long getTotal() {
+		return total;
+	}//getTotal
+
+	public static void setTotal(long total) {
+		Detalle_carrito.total = total;
+	}//setTotal
+
+	@Override
+	public String toString() {
+		return "Detalle_carrito [id=" + id + ", productos_id=" + productos_id + ", carrito_id=" + carrito_id
+				+ ", cantidad=" + cantidad + "]";
+	}//toString
+	
+	
+	
+	
+	
+	
+	
+	
 }
