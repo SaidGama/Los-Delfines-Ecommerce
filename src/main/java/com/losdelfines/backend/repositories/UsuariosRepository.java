188bce203355d07e582d6d03035cf5b61dcea2c8
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-05-12 13:03:44 -0600

    Se agrega usuariosRepository

--- src/main/java/com/losdelfines/backend/repositories/UsuariosRepository.java
+++ src/main/java/com/losdelfines/backend/repositories/UsuariosRepository.java
@@ -1,5 +1,11 @@
 package com.losdelfines.backend.repositories;
 
-public interface UsuariosRepository {
-    
-}
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.losdelfines.backend.models.Usuarios;
+
+@Repository
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
+	Optional<Usuarios> findByCorreo(String correo);
+}//interface usuariosRepository
