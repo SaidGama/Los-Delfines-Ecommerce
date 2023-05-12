d5712cae920dc293b670ea04f85a57e1bee48510
mckey <miguel.angel.alvarez.mdo@gmail.com>
2023-05-12 15:42:57 -0600

    Se actualiza UsuasriosContrasena y se agrega DTO al método update

--- src/main/java/com/losdelfines/backend/controller/UsuariosController.java
+++ src/main/java/com/losdelfines/backend/controller/UsuariosController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.losdelfines.backend.models.CambioContrasena;
 import com.losdelfines.backend.models.Usuarios;
 import com.losdelfines.backend.services.UsuariosServices;
 
@@ -28,33 +30,30 @@ public class UsuariosController {
 	}//constructor
 	
 	@GetMapping
-	public List<Usuarios> GetAllUsers(){
+	public List<Usuarios> GetAllUsuarios(){
 		return usuariosServices.getAllUsuarios();
 	}//getAllUsers
 	
 	@GetMapping(path="{userId}")
-	public Usuarios getUser(@PathVariable("userId") Long id) {
+	public Usuarios getUsuarios(@PathVariable("userId") Long id) {
 		return usuariosServices.getUsuarios(id);
 	}//getUser
 	
 	@DeleteMapping(path="{userId}")
-	public Usuarios deleteUser(@PathVariable("userId") Long id) {
+	public Usuarios deleteUsuarios(@PathVariable("userId") Long id) {
 		return usuariosServices.deleteUsuarios(id);
 	}//deleteUser
 	
 	@PostMapping
-	public Usuarios addUser(@RequestBody Usuarios usuarios) {
+	public Usuarios addUsuarios(@RequestBody Usuarios usuarios) {
 		return usuariosServices.addUsuarios(usuarios);
 	}//addUser
 	
+	//   DTO  DATA Transfer Object	
 	@PutMapping(path="{userId}")
-	public Usuarios updateUser(@PathVariable("userId") Long id,
-			@RequestParam(required = false) String nombre,
-			@RequestParam(required = false) String correo,
-			@RequestParam(required = false) String domicilio,
-			@RequestParam(required = false) String contraseña,
-			@RequestParam(required = false) String telefono) {
-		return usuariosServices.updateUsuarios(id, nombre, correo, domicilio, contraseña, telefono);
+	public Usuarios updateUsuarios(@PathVariable("userId") Long id,
+			@RequestBody CambioContrasena cambioContrasena) {
+		return usuariosServices.updateUsuarios(id, cambioContrasena);
 	}//updateUser
 	
 	
